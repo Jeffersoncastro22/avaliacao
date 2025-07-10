5c16fcb762d3a30bf585d12d4fa27384d81db668
Jeffersoncastro22 <jefferson.castro22@hotmail.com>
2025-07-09 21:03:47 -0300

    Initial commit

---
+++ contato.js
@@ -0,0 +1,13 @@
+
+function submitForm(){
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const dataNascimento = document.getElementById("dataNascimento").value;
+    const idade = document.getElementById("idade").value;
+
+    console.log(nome, email, telefone, dataNascimento, idade)
+
+    
+    
+}
